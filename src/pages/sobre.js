26292e3a76e470955994b0d49e693f64f93463d3
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-03-06 15:10:19 -0300

    aceitarmudanças

--- src/pages/sobre.js
+++ src/pages/sobre.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { View, } from "react-native";
 import HeaderPrincipal from "../components/header";
 // Importa estilos definidos externamente no arquivo stylescomponent
@@ -13,35 +12,6 @@ function Sobre() {
     <View style={styles.container}>
       <HeaderPrincipal />
       <Descricao />
-=======
-import { Text, View, Image } from "react-native";
-// Importa estilos definidos externamente no arquivo stylescomponent
-import styles from "../styles/stylescomponent";
-import { useNavigation } from "@react-navigation/native";
-import HeaderPrincipal from "../components/header";
-
-
-
-function Sobre() {
-
-  const navigation = useNavigation();
-  const imagecircular = { uri: 'https://wallpapercave.com/wp/wp3219866.jpg' }
-
-
-  return (
-    // Componente View representa um contêiner para organizar outros componentes
-    <View style={styles.container}>
-      <HeaderPrincipal/>
-
-      {/* Texto exibido na tela com o estilo definido em styles.titulo */}
-      <Text style={styles.titulo}>Sobre</Text>
-
-      <Image source={imagecircular} style={styles.imagem}>
-
-      </Image>
-      <Text style={styles.descricao}>Este site é sobre florestas, esta imagem mostra uma floresta.</Text>
-
->>>>>>> Stashed changes
     </View>
   );
 }
