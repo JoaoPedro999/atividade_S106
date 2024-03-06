9b08daae3d617abde1437f58f6e778f4395c75c1
Guilherme Veiga Pedromilo <guipedromilo@gmail.com>
2024-03-06 15:28:47 -0300

    update

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
+// Importa o componente StatusBar da biblioteca Expo para controlar a barra de status do aplicativo.
 import { StatusBar } from "expo-status-bar";
+// Importa os componentes View, Text e Button da biblioteca react-native para construir a interface do usuário.
 import { View, Text, Button } from "react-native";
+// Importa os componentes NavigationContainer e useNavigation da biblioteca @react-navigation/native para configurar a navegação entre telas.
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
+// Importa a função createNativeStackNavigator da biblioteca @react-navigation/native-stack para criar um navegador de pilha nativo.
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+// Importa os componentes de páginas do aplicativo.
 import Home from "./src/pages/home";
 import Sobre from "./src/pages/sobre";
 import Contact from "./src/pages/contact";
 import Login from "./src/pages/login";
 
+// Cria uma instância do navegador de pilha usando createNativeStackNavigator.
 const Stack = createNativeStackNavigator();
 
+// Define o componente principal do aplicativo.
 export default function App() {
   return (
+    // Componente NavigationContainer envolve a navegação do aplicativo.
     <NavigationContainer>
+      {/* Navigator contém as telas do aplicativo, cada uma com um nome e um componente associado. */}
       <Stack.Navigator>
+        {/* Define a tela inicial do aplicativo, 'Home', e especifica que o cabeçalho não deve ser mostrado. */}
         <Stack.Screen name="Home" component={Home} options={{headerShown: false,}}/>
+        {/* Define a tela 'Sobre' e especifica que o cabeçalho não deve ser mostrado. */}
         <Stack.Screen name="Sobre" component={Sobre} options={{headerShown: false,}}/>
+        {/* Define a tela 'Contact' e especifica que o cabeçalho não deve ser mostrado. */}
         <Stack.Screen name="Contact" component={Contact} options={{headerShown: false,}}/>
+        {/* Define a tela 'Login' e especifica que o cabeçalho não deve ser mostrado. */}
         <Stack.Screen name="Login" component={Login} options={{headerShown: false,}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
--- src/components/Home/corpohome.js
+++ src/components/Home/corpohome.js
@@ -1,15 +1,23 @@
-//Corpo ------
-
+// Importa os componentes necessários do react-native para construir a interface do usuário.
 import { Text, View, ImageBackground, Button } from 'react-native';
-import styles from '../../styles/styles'; // Importando styles
+// Importa os estilos definidos no arquivo styles.js para estilizar o componente.
+import styles from '../../styles/styles'; 
 
+// Define o componente ContainerHome.
 export default function ContainerHome() {
   return (
-        <View style={styles.secondaryContainer}>
-          <View style={styles.boxcentral}>
-            <Text style= {{color: 'green', fontWeight: 'bold', fontSize: 30, textAlign: 'center', }}>Bem Vindo ao NewSpace</Text>
-            <Text style={{color: 'green', textAlign: 'center',}} >Este site, é destino definitivo para aqules que gostam, exploraram e procuram compreender o vasto e fascinante mundo além do nosso pequeno planeta.</Text>
-    </View>
+    // Define a estrutura de visualização do componente, utilizando estilos definidos.
+    <View style={styles.secondaryContainer}>
+      <View style={styles.boxcentral}>
+        {/* Texto de boas-vindas com estilo personalizado. */}
+        <Text style={{color: 'green', fontWeight: 'bold', fontSize: 30, textAlign: 'center', }}>
+          Bem Vindo ao NatureBrazil
+        </Text>
+        {/* Descrição do site com estilo personalizado. */}
+        <Text style={{color: 'green', textAlign: 'center',}}>
+          Este site é o destino definitivo para aqueles que gostam e deslumbram das belezas deste país tropical.
+        </Text>
+      </View>
     </View>
   );
 }
