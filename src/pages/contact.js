a4dcb6cccc61f6ceeb2c549b367d555dab9636b4
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-03-06 13:29:45 -0300

    commit_Contactfunction

---
+++ src/pages/contact.js
@@ -0,0 +1,23 @@
+import { Button, Text, View } from "react-native-web";
+// Importa estilos definidos externamente no arquivo stylescomponent
+import styles from "../styles/stylescomponent";
+import { useNavigation } from "@react-navigation/native";
+
+
+function Contact() {
+
+    const navigation = useNavigation();
+   return (
+      // Componente View representa um contêiner para organizar outros componentes
+      <View style={styles.body}>
+        
+     {/* Texto exibido na tela com o estilo definido em styles.titulo */}
+     <Text style={styles.titulo}>Home</Text>
+     <Button title="Sobre" onPress={() => navigation.navigate("Sobre")}/>
+        
+      </View>
+        );
+  }
+
+  export default Contact;
+  
