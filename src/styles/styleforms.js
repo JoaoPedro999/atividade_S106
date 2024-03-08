4054a16242dc4facd9c2d638505569eb89efd109
João Pedro Araújo Teixeira <116459774+JoaoPedro999@users.noreply.github.com>
2024-03-08 07:51:01 -0300

    up

--- src/styles/styleforms.js
+++ src/styles/styleforms.js
@@ -4,6 +4,9 @@ const styles = StyleSheet.create({
 
       container: {
         flex: 1,
+        width: "100%",
+        height: "100%",
+        alignItems: "center",
         },
 
         input: {
@@ -27,7 +30,6 @@ const styles = StyleSheet.create({
           flex: 1,
           width: '100%',
           height: '100%',
-          justifyContent: 'center',
           alignItems: 'center',
         },
   });
